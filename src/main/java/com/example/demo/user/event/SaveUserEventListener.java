88118e1b8a16896246fee66c09c0818eeb7d15c7
rbsks <rbsks147@daum.net>
2024-03-14 22:01:34 +0900

    @TransactionalEventListener의 동작원리를 알기 위해 어떤 코드를 봐야하는지 주석에 표시

--- src/main/java/com/example/demo/user/event/SaveUserEventListener.java
+++ src/main/java/com/example/demo/user/event/SaveUserEventListener.java
@@ -4,6 +4,7 @@ import com.example.demo.user.application.domain.SignUpHistory;
 import com.example.demo.user.application.domain.SignUpHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -15,6 +16,10 @@ public class SaveUserEventListener {
 
     private final SignUpHistoryRepository signUpHistoryRepository;
 
+    /*
+     @TransactionalEventListener의 동작 원리는 AbstractPlatformTransactionManager의 processCommit 메서드에서 자세히 확인할 수 있다.
+     */
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void transactionalEventListenerAfterCommit(SaveUserEvent saveUserEvent) {
         log.info("[TransactionalEventListener After Commit]");
